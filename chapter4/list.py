9af368da08590ce0a2d170018a134bf4ce26932a
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2024-10-06 11:59:27 +0600

    updated

---
+++ chapter4/list.py
@@ -0,0 +1,4 @@
+friends=['Apple','orange',5,344.45,False,"Akash","Rohan"]
+
+
+print(friends[1:4])
---
+++ chapter4/list_mthods.py
@@ -0,0 +1,28 @@
+# friends=['Apple','orange',5,344.45,False,"Akash","Rohan"]
+# print(friends)
+# friends.append("shahriar")
+# print(friends)
+
+# l1=[1,6,4,3,8,6]
+# l1.sort()
+# print(l1)
+
+# l1=[1,6,4,3,8,6]
+# l1.reverse()
+# print(l1)
+
+# l1=[1,6,4,3,8,6]
+# l1.insert(3,3333)
+# print(l1)
+
+# l1=[1,6,4,3,8,6]
+# l1.insert(3,3333)
+# print(l1)
+
+# l1=[1,6,4,3,8,6]
+# l2=l1.pop(2)
+# print(l2)
+
+l1=[1,6,4,3,8,6]
+l1.remove(6)
+print(l1)
